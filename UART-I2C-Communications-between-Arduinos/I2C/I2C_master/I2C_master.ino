19f41aa162bd7aec58e1527a80abe584497397dd
Naman Chauhan <29328764+chauhannaman98@users.noreply.github.com>
2018-06-29 17:46:00 +0530

    Create I2C_master.ino

---
+++ UART-I2C-Communications-between-Arduinos/I2C/I2C_master/I2C_master.ino
@@ -0,0 +1,18 @@
+// Include the standard Wire library for I2C
+#include <Wire.h>
+
+int x = 0;
+
+void setup() {
+  // Start the I2C Bus as Master
+  Wire.begin();
+}
+
+void loop() {
+  Wire.beginTransmission(9); // transmit to device #9
+  Wire.write(x); // sends x
+  Wire.endTransmission(); // stop transmitting
+  x++; // Increment x
+  if (x > 5) x = 0; // reset x once it gets 6
+  delay(500);
+}
