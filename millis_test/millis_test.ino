06d9ad6ec31c1cb9636faac3063232c0ccc41a27
Naman Chauhan <chauhannaman98@gmail.com>
2019-07-03 20:15:55 +0530

    Update for multiple variables

--- millis_test/millis_test.ino
+++ millis_test/millis_test.ino
@@ -1,3 +1,5 @@
+const int n = 2;    //number of variables
+
 unsigned long Dx_ON[2] = {15000, 10000};
 unsigned long Ex_ON[2] = {0, 0};
 
@@ -5,14 +7,17 @@ boolean exFlag[2] = {true, true};
 
 void setup() {
   
-  Serial.begin(9600);
-  
+    Serial.begin(9600);
 }
 
 void loop() {
 
+   unsigned long currentMillis = millis();
+   Serial.print("| Millis = ");
+   Serial.print(currentMillis);
+
     //Starting elapsed time for both variables
-    for(int i = 0; i<2; i++)  {
+    for(int i = 0; i<n; i++)  {
       
       if(Ex_ON[i] < Dx_ON[i] && exFlag[i] == true)  {
         record_Ex_ON(i);     
@@ -20,7 +25,7 @@ void loop() {
    }
 
    //Checking stop condition for both variables
-   for(int i = 0; i<2; i++)  {
+   for(int i = 0; i<n; i++)  {
 
      if(Ex_ON[i] >= Dx_ON[i]) {
         Serial.print("\n*****Calling stop function for ");
@@ -30,34 +35,33 @@ void loop() {
      }
    }
 
-   unsigned long currentMillis = millis();
-   Serial.print("   |   Millis = ");
-   Serial.println(currentMillis);
+   Serial.println();
 }
 
 
 
 // user-defined functions here:
+//function for recording elapsed time
 void record_Ex_ON(int i) {
 
     Ex_ON[i] = millis();
-    Serial.print("   |   Calling record function for ");
+    Serial.print(" | Calling record function for ");
     Serial.print(i+1);
-    Serial.print("     E");
+    Serial.print(" | E");
     Serial.print(i+1);
     Serial.print("_ON = ");
     Serial.print(Ex_ON[i]);
-    
 }
 
+//function for stopping elapsed time
 void stop_Ex_ON(int i)  {
 
-  //save elapsedTime to EEPROM first
-  //ENTER EEPROM CODE HERE:
-  Ex_ON[i] = 0;
-  exFlag[i] = false;
-  Serial.println("     Stopped E");
-  Serial.print(i+1);
-  Serial.print("_ON = ");
-  
+    //save elapsedTime to EEPROM first
+    //ENTER EEPROM CODE HERE:
+    Ex_ON[i] = 0;
+    exFlag[i] = false;
+    Serial.print("   STOPPED E");
+    Serial.print(i+1);
+    Serial.print("_ON = ");
+    Serial.println(Ex_ON[i]);
 }
