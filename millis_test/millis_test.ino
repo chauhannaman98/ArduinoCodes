270fb1c9be7f34b08e8a5695e5ad71212fe42314
Naman Chauhan <chauhannaman98@gmail.com>
2019-07-03 19:06:06 +0530

    Replacing with functions

--- millis_test/millis_test.ino
+++ millis_test/millis_test.ino
@@ -1,44 +1,46 @@
-const int ledPin = LED_BUILTIN;
+unsigned long Dx_ON= 15000;
+unsigned long Ex_ON= 0;
 
-//taking duration of 30 sec = 30000 ms
-unsigned long D1_ON = 30000;  
-unsigned long E1_ON = 0;
+boolean exFlag = true;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  
-  unsigned long currentMillis = millis();
-  Serial.print("Millis: ");
-  Serial.print(currentMillis);
-  Serial.print("   D1_ON: ");
-  Serial.print(D1_ON);
-  Serial.print("   E1_ON: ");
-  Serial.println(E1_ON);
-  
-  /*Case 1:
-  * When current time is less than duration,
-  * add time to elapsed time and
-  * print on the serial monitor
-  * Keep led pin ON
-  */
-  if(currentMillis <= D1_ON)  {
-    E1_ON = currentMillis;
-    digitalWrite(ledPin, LOW);
-  }
+  // put your main code here, to run repeatedly:
+    
 
-  /*Case 2:
-  * When current time is more than or equal to duration,
-  * don't add time to elapsed time and
-  * print on the serial monitor
-  */
-  if(currentMillis > D1_ON) {
-    //Duration completed!LED switchedd OFF
-    digitalWrite(ledPin, HIGH);
+    if(Ex_ON < Dx_ON && exFlag == true){
+      
+      record_Ex_ON(exFlag);
+      Serial.print("     E1_ON = ");
+      Serial.print(Ex_ON);
+      Serial.print("     D1_ON: ");
+      Serial.println(Dx_ON);
+    
+    }
+    
+    if(Ex_ON >= Dx_ON) {
+      Serial.print("Calling stop function");
+      stop_Ex_ON();
   }
+}
 
+void record_Ex_ON(boolean flag) {
+
+    Ex_ON = millis();
+    Serial.print("Calling record function");
+
+}
+
+void stop_Ex_ON()  {
+
+  //save elapsedTime to EEPROM first
+  //ENTER EEPROM CODE HERE:
+  Ex_ON = 0;
+  exFlag = false;
+  Serial.println("     Stopped E1_ON");
+  
 }
