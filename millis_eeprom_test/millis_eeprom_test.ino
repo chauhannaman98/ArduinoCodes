711b59235e82fdc82d94df56b9ce809c53b185ef
Naman Chauhan <chauhannaman98@gmail.com>
2019-07-10 19:48:12 +0530

    Minimizing for loop in setup()

--- millis_eeprom_test/millis_eeprom_test.ino
+++ millis_eeprom_test/millis_eeprom_test.ino
@@ -15,73 +15,27 @@ unsigned long Ex_Start[2] = {0, 0};   //stores time when variable is triggered
 boolean rFlag[2] = {false, false};       //flag to  record of Ex_ON
 boolean trigFlag[2] = {true, true};     //flag to trigger  Ex_ON
 
-//int choice;
-int address = 0;
 
 void setup() {
   
     Serial.begin(9600);
-    EEPROM.begin(64);
-    /*Serial.println("1. Trigger 1");
-    Serial.println("2. Trigger 2");
-    Serial.print("Enter choice (1/2): ");*/
+    EEPROM.begin(64);   //
 
     //Reading elapsed time from EEPROM
-    for(int i = 0; i<2; i++)  {
+    for(int i = 0; i<n; i++)  {
        if(i==0)
-          address = 0;        //saving Ex_ON of 1 on 0 location
+          address = 0; 
        if(i==1)
-          address = 5;     //saving Ex_ON of 2 on 5 location
-      Ex_ON[i] = EEPROMRead(address);
-    }
-
-    //Reading duration time from EEPROM
-    for(int i = 0; i<2; i++)  {
-       if(i==0)
-          address = 10;        //saving Dx_ON of 1 on 10 location
-       if(i==1)
-          address = 15;     //saving Dx_ON of 2 on 15 location
-      Dx_ON[i] = EEPROMRead(address);
-    }
-    
-    //Reading state from EEPROM
-    for(int i = 0; i<2; i++)  {
-       if(i==0)
-          address = 20;        //saving Ex_ON of 1 on 0 location
-       if(i==1)
-          address = 25;     //saving Ex_ON of 2 on 32 location
-      Appln_State[i] = EEPROMRead(address);
+          address = 5;
+      Ex_ON[i] = EEPROMRead(address);               //Reading elapsed time from EEPROM
+      Dx_ON[i] = EEPROMRead(address+10);        //Reading duration time from EEPROM
+      Appln_State[i] = EEPROMRead(address)+20;       //Reading state from EEPROM
     }
 }
 
 void loop() {
 
-   //Taking input from user in serial monitor
-  /*if (Serial.available() > 0)
-     int choice = Serial.parseInt();
-  
-  switch(choice)  {
-
-    case 1: if(trigFlag[0])
-                    trigger(0);
-                 else
-                    Serial.println("Variable 1 is already triggered");
-                 Serial.println("\nVariable 1 triggered for 15 seconds");
-                 break;
-                
-    case 2: if(trigFlag[1])
-                    trigger(1);
-                 else
-                    Serial.println("Variable 1 is already triggered");
-                 Serial.println("\nVariable 2 triggered for 10 seconds");
-                 break;
-   }*/
-
    trigger(0);
-
-   //unsigned long currentMillis = millis();
-   //Serial.print("\nMillis = ");
-   //Serial.println(millis());
   
     //Starting elapsed time for both variables
     for(int i = 0; i<n; i++)  {
